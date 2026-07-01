31195f2a9cd6fff99e50232ec34651b1c760f534
Thaveesha Vithana <t.s.vithana@gmail.com>
2026-07-01 12:48:45 +0530

    initialized the client size with react vite and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-amber-500">Pipeline Ready</h1>
+    </div>
+  )
+}
