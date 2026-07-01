1e7b33e4f56546928dc9d3808ec2ec9ceb9c7363
Thaveesha Vithana <t.s.vithana@gmail.com>
2026-07-01 13:25:03 +0530

    add user lgoin to controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -31,3 +31,30 @@ const registerUser = async (req, res) => {
         res.status(500).json({ message: "Server error during registration. ", error })
     }
 };
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found. " });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials. " });
+        }
+
+        generateToken(res, user._id);
+
+        res.status(200).json({
+            message: "Logged in successfully",
+            user: { id: user._id, name: user.name, email: user.email }
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error during login. ", error });
+    }
+};
+
+module.exports = { registerUser, loginUser };
