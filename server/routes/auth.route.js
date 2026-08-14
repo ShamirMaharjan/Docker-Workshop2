1249c9274bc737686f59274783508579b686c295
Shamir Maharjan <shamirmaharjan07@gmail.com>
2026-08-14 09:29:51 +0545

    add s3 bucket cicd

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, googleLogin, forgotPassword, resetPassword } = require('../controllers/auth.controller');
 const { authLimiter } = require('../middlewares/rateLimiter');
-//commentsssds
+//commentsssdsss
 
 router.post('/register', authLimiter, registerUser);
 router.post('/login', authLimiter, loginUser);
